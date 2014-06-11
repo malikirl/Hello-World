eb5fe78e816144117342af61be5d08d7fcf2dcb2
Philip Malik <napiorkowskif@gmail.com>
2014-06-11 14:59:20 +0100

    Int addition

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,10 @@ public class HelloWorld
 	public static void main(String [] args) 
 	{
 		System.out.println("Hello World");
+		int a = 0;
+		int b = 2;
+		int c = a + b;
+
+		System.out.println(c);
 	}
 }
