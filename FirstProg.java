1f2abfaad9410381ac1d8ac8ac87f8c49cd5b88f
Philip Malik <napiorkowskif@gmail.com>
2014-06-11 15:01:58 +0100

    Addition to the program

---
+++ FirstProg.java
@@ -0,0 +1,16 @@
+public class FirstProg
+{
+	public static void main(String [] args)
+	{
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+	}
+
+}
